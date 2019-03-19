6a8a2623e47bafa896277a7d570eb15214052857
bluelamar <bluelamar@gmail.com>
2019-03-18 22:34:08 -0700

    Added cookiejar to db client

--- db.go
+++ db.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"strconv"
+	//"golang.org/x/net/publicsuffix"
 	// FIX couchdb "github.com/leesper/couchdb-golang"
 )
 
 type DBInterface struct {
 	// FIX dbimpl *couchdb.Server
 	baseUrl string
-	cookies []*http.Cookie
+	// cookies []*http.Cookie
+	//authCookie *http.Header
+	client *http.Client
 }
 
 func NewDatabase(cfg *PinoyConfig) (*DBInterface, error) {
@@ -32,7 +38,14 @@ func NewDatabase(cfg *PinoyConfig) (*DBInterface, error) {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payLoad))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Accept", "application/json")
-	client := &http.Client{}
+
+	jar, err := cookiejar.New(nil) // &cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := &http.Client{Jar: jar}
+	dbint.client = client
+
 	resp, err := client.Do(req)
 	// FIX svr, err := couchdb.NewServer("http://root:password@localhost:5984/")
 	// curl -c cdbcookies -H "Accept: application/json" -H "Content-Type: application/x-www-form-urlencoded"  http://localhost:5984/_session -X POST -d "name=wsruler&password=oneringtorule"
@@ -40,8 +53,23 @@ func NewDatabase(cfg *PinoyConfig) (*DBInterface, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	/* FIX
 	cookies := resp.Cookies()
-	dbint.cookies = cookies
+	//dbint.cookies = cookies // []*http.Cookie
+	//cookie := cookies.Get("Set-Cookie")
+	if cookies != nil {
+		for _, cookie := range cookies {
+			if cookie != nil && cookie.Name == "Set-Cookie" {
+				// HAVE: the auth session cookie
+				token := cookie.Value
+				//token := strings.Split(tokenPart, "=")[1]
+				cookieAuthHeader := &http.Header{}
+				cookieAuthHeader.Add("Cookie", fmt.Sprintf("AuthSession=%s", token))
+				dbint.authCookie = cookieAuthHeader
+				break
+			}
+		}
+	} */
 
 	fmt.Println("response Status:", resp.Status)
 	fmt.Println("response Headers:", resp.Header)
@@ -54,27 +82,42 @@ func NewDatabase(cfg *PinoyConfig) (*DBInterface, error) {
 	return &dbint, nil
 }
 
-func (*DBInterface) create(entity string, val interface{}) error {
+func (dbi *DBInterface) create(entity string, val interface{}) error {
 	// TODO create: POST -H "Content-Type: application/json" -H "Accept: application/json" http://localhost:8080/v1/link/${ENTITY} -d "${DATA}"
 	return nil
 }
 
-func (*DBInterface) read(entity string) (interface{}, error) {
+func (dbi *DBInterface) read(entity string) (*map[string]interface{}, error) {
 	// TODO read: -H "Content-Type: application/json" -H "Accept: application/json" http://localhost:8080/v1/link/${ENTITY}/${DBLINK2}
-	return nil, nil
+	url := dbi.baseUrl + entity
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := dbi.client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	log.Println(result)
+
+	return &result, nil
 }
 
-func (*DBInterface) update(entity string, val interface{}) error {
+func (dbi *DBInterface) update(entity string, val interface{}) error {
 	// TODO update: PUT -H "Content-Type: application/json" -H "Accept: application/json" http://localhost:8080/v1/${ENTITY}/${ENV1} -d "${DATA}
 	return nil
 }
 
-func (*DBInterface) delete(key, entity string) error {
+func (dbi *DBInterface) delete(key, entity string) error {
 	// TODO delete: DELETE -H "Accept: application/json" http://localhost:8080/v1/${ENTITY}/${ID}
 	return nil
 }
 
-func (*DBInterface) find() ([]interface{}, error) {
+func (dbi *DBInterface) find() ([]interface{}, error) {
 	// TODO find
 	return nil, nil
 }
